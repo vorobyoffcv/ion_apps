451fb0ce918d516fbe2c0705bb00121deb4d3e60
Constantin <vorobyoffcv@gmail.com>
2018-08-22 17:53:16 +0700

    22

--- cirpichito/src/pages/materials/materials.ts
+++ cirpichito/src/pages/materials/materials.ts
@@ -14,12 +14,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'materials.html',
 })
 export class MaterialsPage {
+  selectedItem: any;
+  icons: string[];
+//  items: Array<{ title: string, note: string, icon: string, length: number, height: number, depth: number, joint: number}>;
+  items: Array<{ title: string, note: string, length: number, height: number, depth: number, joint: number}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.selectedItem = navParams.get('item');
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Brick ' + i,
+        note: 'Red...',
+        length: 250,
+        height: 60,
+        depth: 120,
+        joint: 12
+      });
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MaterialsPage');
   }
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+
+    //    this.navCtrl.push(MaterialsPage, {
+    //      item: item
+    //    });
+
+    this.navCtrl.setRoot(this.constructor.name, {
+      item: item
+    });
+
+
+  }
+
 
 }
